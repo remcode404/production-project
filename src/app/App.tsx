62f36a44e706f926605fa79d8d8705ac6e6c5626
JokerJok <dmehtiev@inbox.ru>
2023-05-10 14:08:55 +0300

    Добавил перевод на английский язык

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,17 +4,20 @@ import { useTheme } from "./providers/ThemeProvider";
 import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
 import { SideBar } from "widgets/SideBar";
+import { Suspense } from "react";
 
 const App = () => {
   const { theme } = useTheme();
 
   return (
     <div className={classNames("app", {}, [theme])}>
-      <Navbar />
-      <div className="content-page">
-        <SideBar />
-        <AppRouter />
-      </div>
+      <Suspense fallback="">
+        <Navbar />
+        <div className="content-page">
+          <SideBar />
+          <AppRouter />
+        </div>
+      </Suspense>
     </div>
   );
 };
--- src/widgets/SideBar/ui/SideBar/SideBar.tsx
+++ src/widgets/SideBar/ui/SideBar/SideBar.tsx
@@ -2,6 +2,7 @@ import { classNames } from "shared/lib/classNames/classNames";
 import cls from "./SideBar.module.scss";
 import { useState } from "react";
 import { ThemeSwitcher } from "widgets/ThemeSwitcher";
+import { LangSwitcher } from "widgets/LangSwitcher/LangSwitcher";
 
 interface SideBarProps {
   className?: string;
@@ -22,7 +23,7 @@ export const SideBar = ({ className }: SideBarProps) => {
       <button onClick={onToggle}>Сверни</button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
-        {/* LangSwitcher */}
+        <LangSwitcher className={cls.lang} />
       </div>
     </div>
   );
